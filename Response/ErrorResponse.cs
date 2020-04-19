68c7ea84d9323c725567b5fefb8fb956165ec971
mkesicki <marc_he_w@poczta.onet.pl>
2020-04-19 21:50:54 +0200

    Add missing files

---
+++ Response/ErrorResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFDCImport.Response
+{
+    public class Error
+    {
+        public string statusCode { get; set; }
+        public string message { get; set; }
+        public List<string> fields { get; set; }
+    }
+
+    public class ResultError
+    {
+        public string referenceId { get; set; }
+        public List<Error> errors { get; set; }
+    }
+
+    public class ErrorResponse
+    {
+        public bool hasErrors { get; set; }
+        public List<ResultError> results { get; set; }
+    }
+}
---
+++ Response/SuccessResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFDCImport.Response
+{
+    public class ResultSuccess
+    {
+        public string referenceId { get; set; }
+        public string id { get; set; }
+    }
+
+    public class SuccessResponse
+    {
+        public bool hasErrors { get; set; }
+        public List<ResultSuccess> results { get; set; }
+    }
+}
